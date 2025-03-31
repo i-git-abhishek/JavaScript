ee2f80bc2daf46951699cb970b0c774ec111f3c6
i-git-abhishek <ggolu1701@gmail.com>
2025-03-31 16:27:54 +0000

    new concepts

---
+++ 01_Basics/Strings.js
@@ -0,0 +1,8 @@
+let name = "The quick Brown Fox Jumps over the lazy dog"
+let index = 5
+
+console.log(`The character at index ${index} is ${name.split(' ')}`);
+console.log(name.split(' '))
+
+// const gameName = new String("AbhishekA")
+// console.log(gameName.slice(-7,-2));
