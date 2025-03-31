514163fcb39ad04d4e178eac2394795826dca195
i-git-abhishek <ggolu1701@gmail.com>
2025-03-31 14:36:36 +0000

    2 new concepts

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,6 @@
+const accID = "1234"
+let accName = "Abhishek"
+var accMail = "Abhi@gmail.com"
+let accState
+
+console.table([accID, accName, accMail, accState]);
---
+++ 01_Basics/Conversion_ops.js
@@ -0,0 +1,6 @@
+let score = ""
+console.log(typeof(score))
+
+let value = Boolean(score)
+console.log(typeof(value))
+console.log((value))
